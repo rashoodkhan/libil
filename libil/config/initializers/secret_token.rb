0953868c8e21e8fce09cb5d00d989aaf97ace5ef
Rashid Khan <rashood.khan@gmail.com>
2013-09-01 00:51:36 +0530

    Initial Commit of Libil

---
+++ libil/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Libil::Application.config.secret_key_base = '918bdac761200bd970658f20487f8f8e68e848a1a4417fa263f2792e213cf23f51ffb0132014a79b630452c4f9ebbd7d3f76a6f087ba54e16ae764e344e2dfa1'
